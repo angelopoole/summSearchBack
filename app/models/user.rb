bcdd397e760cfc5ddf8362f4da876f48a7c7dbc8
angelopoole <angelo.poole@outlook.com>
2020-07-01 15:49:45 -0400

    auth and selfjoins built

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: true
+    validates :username, presence: true
 
     has_many :followed_users, foreign_key: :follower_id, class_name: 'Follow'
-
     has_many :followees, through: :followed_users
-
+    # above allows following, below allows followers 
     has_many :following_users, foreign_key: :followee_id, class_name: 'Follow'
     has_many :followers, through: :following_users
 
+    has_secure_password
 end
